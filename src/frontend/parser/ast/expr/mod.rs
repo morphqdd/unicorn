3f1f7bd550e3951da30b0d10789f55e614be109e
devil <europedreadlydevil@gmail.com>
2025-08-18 15:56:23 +0500

    Refactor: Enhance AST and add parser for functions and assignments
    This commit introduces function and assignment parsing capabilities.
    It expands the AST to represent functions, function types, and assignment statements.

--- src/frontend/parser/ast/expr/mod.rs
+++ src/frontend/parser/ast/expr/mod.rs
@@ -3,7 +3,18 @@ pub enum Expr {
     Ident(String),
     Call {
         ident: Box<Expr>,
-        args: Vec<Expr>
+        args: Vec<Expr>,
     },
-    Lit(String)
+    Lit(String),
+    Function {
+        name: Box<Expr>,
+        function_ty: Box<Expr>,
+        body: Vec<Expr>,
+    },
+    FunctionType {
+        params: Vec<(Expr, Expr)>,
+        ret_ty: Box<Expr>,
+    },
+    Assign((Box<Expr>, Box<Expr>), Box<Expr>),
+    GlobalDataAddr(Box<Expr>)
 }
--- src/frontend/parser/mod.rs
+++ src/frontend/parser/mod.rs
@@ -1,23 +1,124 @@
-use peg::*;
-use crate::frontend::parser::ast::expr::Expr;
+use crate::frontend::parser::ast::{expr::Expr};
 pub use crate::frontend::parser::parser::*;
+use peg::*;
 
 pub mod ast;
 
 parser! {
     pub grammar parser() for str {
-        pub rule number() -> Expr = n:$(['0'..='9']+) { Expr::Lit(n.to_string()) }
-        rule _() = quiet!{[' ' | '\t']*}
+        pub rule function() -> Expr
+            = _ name:ident() _ ":" _ t:ty() _ "{" _ body:exprs() _ "}" _
+            { Expr::Function { name: Box::new(name), function_ty: Box::new(t), body } }     
+        rule function_ty() -> Expr 
+            = _ params:(( i:expr() "(" _ t:ty() _ ")" { (i, t) }) ** " ")
+            _ "->" _ ret_ty:(_ i:ident() _ {i}) { Expr::FunctionType { params, ret_ty: Box::new(ret_ty) } }
+        rule exprs() -> Vec<Expr> = e:(expr() ** _) { e }
+        rule expr() -> Expr = function() / assign() /ident() / literal()
+        rule assign() -> Expr = _ "let" _ i:ident() _ ":" _ t:ty() _ "=" _ e:expr() { Expr::Assign((Box::new(i), Box::new(t)), Box::new(e)) }  
+        rule ty() -> Expr = function_ty() / ident() 
+        rule ident() -> Expr
+            = quiet!{ n:$(['a'..='z' | 'A'..='Z' | '_']['a'..='z' | 'A'..='Z' | '0'..='9' | '_']*)
+            { Expr::Ident(n.to_owned()) } }
+        / expected!("identifier")
+        rule literal() -> Expr
+            = n:$(['0'..='9']+) { Expr::Lit(n.to_owned()) }
+            / "&" i:ident() { Expr::GlobalDataAddr(Box::new(i)) }
+        rule _() = quiet!{[' ' | '\t' | '\n']*}
     }
 }
 
-
 #[cfg(test)]
 mod test {
+    use std::vec;
+
     use crate::frontend::parser::{self, ast::expr::Expr};
 
     #[test]
-    fn number_parse() {
-        assert_eq!(parser::number("32"), Ok(Expr::Lit("32".into())))
+    fn simple_function_parse() {
+        assert_eq!(
+            parser::function("foo : a(T) b(None) -> nil {}"),
+            Ok(Expr::Function {
+                name: Box::new(Expr::Ident(String::from("foo"))),
+                function_ty: Box::new(
+                    Expr::FunctionType { 
+                        params: vec![
+                            (Expr::Ident(String::from("a")), Expr::Ident(String::from("T"))),
+                            (Expr::Ident(String::from("b")), Expr::Ident(String::from("None")))
+                        ],
+                        ret_ty: Box::new(Expr::Ident(String::from("nil"))),
+                    }
+                ),
+                body: vec![]
+            })
+        )
+    }
+
+    #[test]
+    fn complicated_function_parse() {
+        assert_eq!(
+            parser::function("foo : bar( a(i32) b(i32) -> nil ) -> nil { buzz : a(i32) b(i32) -> nil {} nil }"),
+            Ok(Expr::Function {
+                name: Box::new(Expr::Ident("foo".into())),
+                function_ty: Box::new(Expr::FunctionType { 
+                    params: vec![
+                        (
+                            Expr::Ident("bar".into()),
+                            Expr::FunctionType {
+                                params: vec![
+                                    (Expr::Ident("a".into()), Expr::Ident("i32".into())), 
+                                    (Expr::Ident("b".into()), Expr::Ident("i32".into()))
+                                ],
+                                ret_ty: Box::new(Expr::Ident("nil".into())),
+                            }
+                        )
+                    ],
+                    ret_ty: Box::new(Expr::Ident("nil".into())),
+                }),
+                body: vec![
+                    Expr::Function {
+                        name: Box::new(Expr::Ident("buzz".into())),
+                        function_ty: Box::new(Expr::FunctionType { 
+                            params: vec![
+                                (Expr::Ident("a".into()), Expr::Ident("i32".into())), 
+                                (Expr::Ident("b".into()), Expr::Ident("i32".into()))
+                            ],
+                            ret_ty: Box::new(Expr::Ident("nil".into())),
+                        }),
+                        body: vec![]
+                    },
+                    Expr::Ident("nil".into())
+                ]
+            })
+        )
+    }
+
+    #[test]
+    fn let_expr_parse() {
+        assert_eq!(
+            parser::function("main : -> nil { let a : i32 = 20 nil } "),
+            Ok(Expr::Function { 
+                name: Box::new(Expr::Ident("main".into())), 
+                function_ty: Box::new(Expr::FunctionType { 
+                    params: vec![], 
+                    ret_ty: Box::new(Expr::Ident("nil".into())) 
+                }), 
+                body: vec![
+                    Expr::Assign((Box::new(Expr::Ident("a".into())), Box::new(Expr::Ident("i32".into()))), Box::new(Expr::Lit("20".into()))),
+                    Expr::Ident("nil".into())
+                ]
+            })
+        )
+    }
+
+    #[test]
+    fn some_strange_things() {
+        assert_eq!(
+            parser::function("main : i32 {} "),
+            Ok(Expr::Function {
+                name: Box::new(Expr::Ident("main".into())), 
+                function_ty: Box::new(Expr::Ident("i32".into())),
+                body: vec![]
+            })
+        )
     }
 }
