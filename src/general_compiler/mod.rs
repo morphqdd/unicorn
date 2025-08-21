1154d43bbbc4ba1512a7558e36fcd7867daf64cb
devil <europedreadlydevil@gmail.com>
2025-08-22 01:11:37 +0500

    Refactor: Introduce type definition system
    This commit introduces a basic type definition system and a simple
    example of memory allocation using `malloc` with simple read and write.

--- src/general_compiler/mod.rs
+++ src/general_compiler/mod.rs
@@ -11,7 +11,11 @@ use cranelift::{
 };
 use std::{collections::HashMap, ops::DerefMut};
 use cranelift::codegen::ir::BlockArg;
-use cranelift::prelude::IntCC;
+use cranelift::codegen::ir::Fact::Mem;
+use cranelift::prelude::{IntCC, MemFlags};
+use crate::general_compiler::type_def::{Field, TypeDef};
+
+mod type_def;
 
 const REDUCTIONS_LIMIT: i64 = 2;
 
@@ -29,92 +33,17 @@ pub trait GeneralCompiler<T: Module> {
     where
         Self: Sized,
     {
+
+
         let (mut builder_ctx, mut ctx, data_description, mut module) = self.unwrap();
 
-        let int= module.target_config().pointer_type();
-        ctx.func.signature.returns.push(AbiParam::new(int));
+        let ty_vec = TypeDef::new("Vec", vec![
+            Field::new("ptr", module.target_config().pointer_type()),
+            Field::new("len", module.target_config().pointer_type()),
+            Field::new("cap", module.target_config().pointer_type())
+        ]);
 
-        let mut builder = FunctionBuilder::new(&mut ctx.func, &mut builder_ctx);
-
-        let entry_block = builder.create_block();
-        builder.append_block_params_for_function_params(entry_block);
-        builder.switch_to_block(entry_block);
-        builder.seal_block(entry_block);
-
-        let reductions_limit = builder.declare_var(int);
-        let reductions_variable = builder.declare_var(int);
-        let limit_of_reductions = builder.ins().iconst(int, REDUCTIONS_LIMIT);
-        let default_reductions = builder.ins().iconst(int, 0);
-        builder.def_var(reductions_limit, limit_of_reductions);
-        builder.def_var(reductions_variable, default_reductions);
-
-        let cond_block = builder.create_block();
-        builder.ins().jump(cond_block, &[]);
-        builder.switch_to_block(cond_block);
-
-        let limit_of_reductions = builder.use_var(reductions_limit);
-        let current_reductions = builder.use_var(reductions_variable);
-        let condition_value = builder.ins().icmp(IntCC::Equal, current_reductions, limit_of_reductions);
-
-        let then_block = builder.create_block();
-        let else_block = builder.create_block();
-        let merge_block = builder.create_block();
-
-        builder.append_block_param(merge_block, int);
-
-        builder
-            .ins()
-            .brif(
-                condition_value,
-                then_block, &[],
-                else_block, &[]
-            );
-
-        builder.switch_to_block(then_block);
-        builder.seal_block(then_block);
-        let then_result = builder.ins().iconst(int, 10);
-        builder.def_var(reductions_variable, default_reductions);
-        builder.ins().jump(merge_block, &[BlockArg::Value(then_result)]);
-
-        builder.switch_to_block(else_block);
-        builder.seal_block(else_block);
-        let else_result = builder.ins().iconst(int, 20);
-        let the_one = builder.ins().iconst(int, 1);
-        let current_reductions = builder.use_var(reductions_variable);
-        let the_new_val = builder.ins().iadd(current_reductions,the_one);
-        builder.def_var(reductions_variable, the_new_val);
-        builder.ins().jump(merge_block, &[BlockArg::Value(else_result)]);
-
-
-        builder.switch_to_block(merge_block);
-        builder.seal_block(merge_block);
-
-        let mut sig = module.make_signature();
-
-        sig.params.push(AbiParam::new(int));
-        sig.returns.push(AbiParam::new(int));
-
-        let callee = module
-            .declare_function("stdprint", Linkage::Import, &sig)?;
-
-        let local_callee = module.declare_func_in_func(callee, builder.func);
-
-        let arg = builder.use_var(reductions_variable);
-        let block_param = *builder.block_params(merge_block).get(0).unwrap();
-        let call = builder.ins().call(local_callee, &[arg]);
-        let call = builder.ins().call(local_callee, &[block_param]);
-
-        builder.ins().jump(cond_block, &[]);
-        builder.seal_block(cond_block);
-
-        builder.finalize();
-
-        let id =
-            module.declare_function("main", Linkage::Export, &ctx.func.signature)?;
-
-        module.define_function(id, &mut ctx)?;
-
-        module.clear_context(&mut ctx);
+        //let mut funcs = vec![];
 
         // for expr in exprs {
         //     match expr {
@@ -189,8 +118,14 @@ pub trait GeneralCompiler<T: Module> {
         //                     panic!("Not a name!")
         //                 };
         //
-        //                 let id =
-        //                     module.declare_function(&name, Linkage::Export, &ctx.func.signature)?;
+        //                 let id = module
+        //                     .declare_function(
+        //                         &format!("_{name}_"),
+        //                         Linkage::Export,
+        //                         &ctx.func.signature
+        //                     )?;
+        //
+        //                 funcs.push(format!("_{name}_"));
         //
         //                 module.define_function(id, &mut ctx)?;
         //
@@ -201,6 +136,133 @@ pub trait GeneralCompiler<T: Module> {
         //         _ => panic!("Translation not support yet"),
         //     }
         // }
+        let int= module.target_config().pointer_type();
+        ctx.func.signature.returns.push(AbiParam::new(int));
+
+        let mut builder = FunctionBuilder::new(&mut ctx.func, &mut builder_ctx);
+
+        let entry_block = builder.create_block();
+        builder.append_block_params_for_function_params(entry_block);
+        builder.switch_to_block(entry_block);
+        builder.seal_block(entry_block);
+
+        let size_of_element = 8;
+        let cap = 16;
+        let buffer_size= builder.ins().iconst(int, size_of_element * cap);
+
+        let mut malloc_sig = module.make_signature();
+        malloc_sig.params.push(AbiParam::new(int));
+        malloc_sig.returns.push(AbiParam::new(int));
+
+        let callee_malloc = module
+            .declare_function("malloc", Linkage::Import, &malloc_sig)
+            .unwrap();
+        let local_callee_malloc= module
+            .declare_func_in_func(callee_malloc, builder.func);
+
+        let call = builder.ins().call(local_callee_malloc, &[buffer_size]);
+        let ptr = *builder.inst_results(call).get(0).unwrap();
+        let zero = builder.ins().iconst(int, 0);
+        builder.ins().store(MemFlags::new(), zero, ptr, 0);
+        builder.ins().store(MemFlags::new(), zero, ptr, 8);
+
+
+        let reductions_limit = builder.declare_var(int);
+        let reductions_variable = builder.declare_var(int);
+        let limit_of_reductions = builder.ins().iconst(int, REDUCTIONS_LIMIT);
+        let default_reductions = builder.ins().iconst(int, 0);
+        builder.def_var(reductions_limit, limit_of_reductions);
+        builder.def_var(reductions_variable, default_reductions);
+
+        let cond_block = builder.create_block();
+        builder.append_block_param(cond_block, int);
+        builder.ins().jump(cond_block, &[BlockArg::Value(ptr)]);
+        builder.switch_to_block(cond_block);
+
+        let limit_of_reductions = builder.use_var(reductions_limit);
+        let current_reductions = builder.use_var(reductions_variable);
+        let condition_value = builder.ins().icmp(IntCC::Equal, current_reductions, limit_of_reductions);
+
+        let then_block = builder.create_block();
+        let else_block = builder.create_block();
+        let merge_block = builder.create_block();
+
+        builder.append_block_param(then_block, int);
+        builder.append_block_param(else_block, int);
+        builder.append_block_param(merge_block, int);
+        builder.append_block_param(merge_block, int);
+
+        let ptr = *builder.block_params(cond_block).get(0).unwrap();
+
+        builder
+            .ins()
+            .brif(
+                condition_value,
+                then_block, &[BlockArg::Value(ptr)],
+                else_block, &[BlockArg::Value(ptr)]
+            );
+
+        builder.switch_to_block(then_block);
+        builder.seal_block(then_block);
+
+        builder.def_var(reductions_variable, default_reductions);
+
+        let ptr = *builder.block_params(then_block).get(0).unwrap();
+        let tmp_val = builder.ins().load(int, MemFlags::new(), ptr, 0);
+        let the_one = builder.ins().iconst(int, 1);
+        let new_val = builder.ins().iadd(tmp_val, the_one);
+        builder.ins().store(MemFlags::new(), new_val, ptr, 0);
+        builder.ins().jump(merge_block, &[BlockArg::Value(ptr), BlockArg::Value(the_one)]);
+
+        builder.switch_to_block(else_block);
+        builder.seal_block(else_block);
+        let the_one = builder.ins().iconst(int, 1);
+        let the_two = builder.ins().iconst(int, 2);
+        let ptr = *builder.block_params(else_block).get(0).unwrap();
+        let tmp_val = builder.ins().load(int, MemFlags::new(), ptr, 0);
+        let tmp_val_2 = builder.ins().load(int, MemFlags::new(), ptr, 8);
+        let new_val = builder.ins().iadd(tmp_val, tmp_val_2);
+        builder.ins().store(MemFlags::new(), new_val, ptr, 8);
+        let current_reductions = builder.use_var(reductions_variable);
+        let the_new_val = builder.ins().iadd(current_reductions,the_one);
+        builder.def_var(reductions_variable, the_new_val);
+        builder.ins().jump(merge_block, &[BlockArg::Value(ptr), BlockArg::Value(the_two)]);
+
+
+        builder.switch_to_block(merge_block);
+        builder.seal_block(merge_block);
+
+        let mut sig = module.make_signature();
+
+        sig.params.push(AbiParam::new(int));
+        sig.returns.push(AbiParam::new(int));
+
+        let callee = module
+            .declare_function("stdprint", Linkage::Import, &sig)?;
+
+        let local_callee = module.declare_func_in_func(callee, builder.func);
+
+        let ptr = *builder.block_params(merge_block).get(0).unwrap();
+        let branch = *builder.block_params(merge_block).get(1).unwrap();
+        let first = builder.ins().load(int, MemFlags::new(), ptr, 0);
+        let second = builder.ins().load(int, MemFlags::new(), ptr, 8);
+        let call = builder.ins().call(local_callee, &[first]);
+        let call = builder.ins().call(local_callee, &[second]);
+        let _ = builder.ins().call(local_callee, &[branch]);
+
+        builder.ins().jump(cond_block, &[BlockArg::Value(ptr)]);
+        builder.seal_block(cond_block);
+
+        builder.finalize();
+
+        let id =
+            module.declare_function("main", Linkage::Export, &ctx.func.signature)?;
+
+        module.define_function(id, &mut ctx)?;
+
+        module.clear_context(&mut ctx);
+
+
         Ok(Self::from_general_compiler(
             builder_ctx,
             ctx,
